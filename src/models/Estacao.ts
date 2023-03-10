03dc41ab54ba3b2df0fc333d8a088352414d30d3
ZeroPirata <gabriel0gsbn@gmail.com>
2023-03-09 22:19:21 -0300

    API20231-44 ✨ Feat: Modelo do banco [Estação]

---
+++ src/models/Estacao.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity({ name: "estacao" })
+export class Estacao {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  nome!: string;
+
+  @Column()
+  lati!: number;
+
+  @Column()
+  long!: number;
+}
---
+++ src/models/index.ts
@@ -0,0 +1,5 @@
+import { Estacao } from "./Estacao";
+
+export {
+    Estacao
+}
