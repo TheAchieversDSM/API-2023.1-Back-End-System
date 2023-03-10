c3e5327bd66d81e369292f44f575a36619640724
ZeroPirata <gabriel0gsbn@gmail.com>
2023-03-09 22:18:33 -0300

    API20231-44 ✨ Feat: Configurações do banco e dotenv

---
+++ src/config/database.ts
@@ -0,0 +1,16 @@
+import { DataSource } from "typeorm";
+import * as dotenv from "dotenv";
+import { Estacao } from "../models";
+
+dotenv.config();
+
+export const DataBaseSource = new DataSource({
+  type: "mysql",
+  host: process.env.HOST,
+  port: Number(process.env.PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
+  synchronize: true,
+  entities: [Estacao],
+});
