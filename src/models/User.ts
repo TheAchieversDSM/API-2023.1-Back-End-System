cacedc2ddaf23c1ac72dcf9ff6ab10d94f9ccffb
ZeroPirata <gabriel0gsbn@gmail.com>
2023-03-14 00:17:47 -0300

    API20231-50 ✨ Feat: Modelo do usuario

---
+++ src/models/User.ts
@@ -0,0 +1,34 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BeforeInsert,
+  BeforeUpdate,
+} from "typeorm";
+import * as bcrypt from "bcrypt";
+
+@Entity({ name: "user" })
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  nome!: string;
+
+  @Column()
+  email!: string;
+
+  @Column()
+  senha!: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword(): void {
+    if (this.senha) {
+      this.senha = bcrypt.hashSync(this.senha, bcrypt.genSaltSync(10));
+    }
+  }
+  compare(input: string): Promise<boolean> {
+    return bcrypt.compare(input, this.senha);
+  }
+}
