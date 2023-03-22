15dc61bd190e1cffe985f93c833c59a53eaa77ad
taissalomao <tais.salomao@fatec.sp.gov.br>
2023-03-22 07:58:25 -0300

    API20231-97 📋 Tarefa: Inserido rota GET para busca de todos os reports e reports por ID

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,4 +4,5 @@ import { User } from "./User";
 import { Parametro } from "./Parametro";
 import { EstacaoParametro } from "./EstacaoParametro";
 import { TipoParametro } from "./TipoParametro";
-export { Alerta, Estacao, User, Parametro, EstacaoParametro, TipoParametro };
+import { Report } from "./Report";
+export { Alerta, Estacao, User, Parametro, EstacaoParametro, TipoParametro, Report };
