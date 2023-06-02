27eebb5fa710eca1d9c0594482ad137004a5eebe
ZeroPirata <Gabriel0gsbn@gmail.com>
2023-06-02 16:20:17 -0300

    🐛 Fix: aws memory controll

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,26 @@
-import { DataBaseSource } from "./config/database";
-import express from "express";
-import cors from "cors";
-import router from "./routes/";
-import { generate } from "./controller/generate";
-import { User } from "./models/index";
-import "./config/firebase";
-import RealTime from "./controller/firebase";
-import { Worker } from "worker_threads";
+import { DataBaseSource } from './config/database';
+import express from 'express';
+import cors from 'cors';
+import router from './routes/';
+import { generate } from './controller/generate';
+import { User } from './models/index';
+import './config/firebase';
 
 const app = express();
+
 const usuarioRepository = DataBaseSource.getRepository(User);
-// No worker principal
-const worker = new Worker(`${__dirname}/controller/firebase.ts`);
-worker.on("message", (result) => {
-  console.log(result);
-});
-worker.on("error", (error) => {
-  console.error(error);
-});
-worker.on("exit", (code) => {
-  console.log(`Worker saiu com código ${code}`);
-});
-worker.postMessage({ action: "startRealTime" });
 try {
   DataBaseSource.initialize()
     .then(async () => {
       const usuarioInicial = await usuarioRepository.findOne({
         where: {
-          email: "usuario1@theAchievers.com",
-        },
+          email: 'usuario1@theAchievers.com'
+        }
       });
       if (!usuarioInicial) {
         generate();
       }
-      console.log("Banco conectado com sucesso");
+      console.log('Banco conectado com sucesso');
     })
     .catch(({ err }) => {
       console.log(err);
@@ -42,7 +29,7 @@ try {
   console.log(error);
 }
 
-app.listen(5000, () => console.log("Server conectado"));
+app.listen(5000, () => console.log('Server conectado'));
 app.use(cors());
 app.use(express.json());
 app.use(router);
